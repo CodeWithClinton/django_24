11cc4dc5917dae74d237b6ac963088f238f0c605
Clinton Nwachukwu <Clintonmatics@gmail.com>
2024-01-06 22:26:23 +0100

    prepping for deplyment

--- django_24/settings.py
+++ django_24/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,6 +120,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR/'static']
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 MEDIA_URL = 'img/'
 MEDIA_ROOT = BASE_DIR/'media'
 
